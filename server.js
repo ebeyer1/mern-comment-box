831c927d62ce2b39d9aa69bc0c0a5c706ea4d8ac
Eric Beyer <eric.m.beyer@gmail.com>
2017-10-11 22:32:44 -0700

    Hook up api to a real mLab database

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 //server.js
-'use strict'
+'use strict';
 //first we import our dependencies…
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var Comment = require('./src/model/comments');
 //and create our instances
 var app = express();
 var router = express.Router();
 //set our port to either a predetermined port number if you have set 
 //it up, or 3001
 var port = process.env.API_PORT || 3001;
+//db config
+mongoose.connect('mongodb://test_user:password@ds117495.mlab.com:17495/mern-comment-box');
 //now we should configure the API to use bodyParser and look for 
 //JSON data in the request body
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -29,6 +32,30 @@ app.use(function(req, res, next) {
 router.get('/', function(req, res) {
     res.json({ message: 'API Initialized!'});
 });
+
+//adding the /comments route to our /api router
+router.route('/comments')
+    // retrieve all comments from the database
+    .get(function(req, res) {
+        //looks at our Comment Schema
+        Comment.find(function(err, comments) {
+            if (err) res.send(err);
+            // responds with a json object of our database comments.
+            res.json(comments);
+        });
+    })
+    //post new comment to the database
+    .post(function(req, res) {
+        var comment = new Comment();
+        //body parser lets us use the req.body
+        comment.author = req.body.author;
+        comment.text = req.body.text;
+    
+        comment.save(function(err) {
+           if (err) res.send(err);
+            res.json({message: 'Comment successfully added!'});
+        });
+    });
 //Use our router configuration when we call /api
 app.use('/api', router);
 //starts the server and listens for requests
