0d819319fd9cdb5f24f393b15efda335045dc1c2
Eric Beyer <eric.m.beyer@gmail.com>
2017-10-25 10:05:32 -0700

    use router again

--- server.js
+++ server.js
@@ -39,66 +39,67 @@ mongoose.connect(mongoUri, mongooseOptions);
 //    next();
 //});
 
-//var router = express.Router();
+var router = express.Router();
 
 //now we can set the route path & initialize the API
-//router.get('/', function(req, res) {
-//    res.json({ message: 'API Initialized!'});
-//});
-//
-////adding the /comments route to our /api router
-//router.route('/comments')
-//    // retrieve all comments from the database
-//    .get(function(req, res) {
-//        //looks at our Comment Schema
-//        Comment.find(function(err, comments) {
-//            if (err) res.send(err);
-//            // responds with a json object of our database comments.
-//            res.json(comments);
-//        });
-//    })
-//    //post new comment to the database
-//    .post(function(req, res) {
-//        var comment = new Comment();
-//        //body parser lets us use the req.body
-//        comment.author = req.body.author;
-//        comment.text = req.body.text;
-//    
-//        comment.save(function(err) {
-//           if (err) res.send(err);
-//            res.json({message: 'Comment successfully added!'});
-//        });
-//    });
-//
-////Adding a route to a specific comment based on the database ID
-//router.route('/comments/:comment_id')
-//    //the put method gives us the chance to update our comment based on
-//    //the ID passed to the route
-//    .put(function(req, res) {
-//        Comment.findById(req.params.comment_id, function(err, comment) {
-//            if (err) res.send(err);
-//            //setting the new author and text to whatever was changed. If
-//            //nothing was changed we will not alter the field.
-//            (req.body.author) ? comment.author = req.body.author : null;
-//            (req.body.text) ? comment.text = req.body.text : null;
-//            //save comment
-//            comment.save(function(err) {
-//                if (err) res.send(err);
-//                res.json({ message: 'Comment has been updated' });
-//            });
-//        });
-//    })
-//    //delete method for removing a comment from our database
-//    .delete(function(req, res) {
-//        //selects the comment by its ID, then removes it.
-//        Comment.remove({ _id: req.params.comment_id }, function(err, comment) {
-//            if (err) res.send(err);
-//            res.json({ message: 'Comment has been deleted' });
-//        });
-//    });
+router.get('/', function(req, res) {
+    res.json({ message: 'API has been Initialized!'});
+});
+
+//adding the /comments route to our /api router
+router.route('/comments')
+    // retrieve all comments from the database
+    .get(function(req, res) {
+        //looks at our Comment Schema
+        Comment.find(function(err, comments) {
+            if (err) res.send(err);
+            // responds with a json object of our database comments.
+            res.json(comments);
+        });
+//        res.json({comments: [], msg: 'ze comments'});
+    })
+    //post new comment to the database
+    .post(function(req, res) {
+        var comment = new Comment();
+        //body parser lets us use the req.body
+        comment.author = req.body.author;
+        comment.text = req.body.text;
+    
+        comment.save(function(err) {
+           if (err) res.send(err);
+            res.json({message: 'Comment successfully added!'});
+        });
+    });
+
+//Adding a route to a specific comment based on the database ID
+router.route('/comments/:comment_id')
+    //the put method gives us the chance to update our comment based on
+    //the ID passed to the route
+    .put(function(req, res) {
+        Comment.findById(req.params.comment_id, function(err, comment) {
+            if (err) res.send(err);
+            //setting the new author and text to whatever was changed. If
+            //nothing was changed we will not alter the field.
+            (req.body.author) ? comment.author = req.body.author : null;
+            (req.body.text) ? comment.text = req.body.text : null;
+            //save comment
+            comment.save(function(err) {
+                if (err) res.send(err);
+                res.json({ message: 'Comment has been updated' });
+            });
+        });
+    })
+    //delete method for removing a comment from our database
+    .delete(function(req, res) {
+        //selects the comment by its ID, then removes it.
+        Comment.remove({ _id: req.params.comment_id }, function(err, comment) {
+            if (err) res.send(err);
+            res.json({ message: 'Comment has been deleted' });
+        });
+    });
 
 //Use our router configuration when we call /api
-//app.use('/api', router);
+app.use('/api', router);
 
 //app.get('/api', (req, res) => {
 //    res.json({ message: 'API Initialized!!!'});
@@ -118,18 +119,18 @@ mongoose.connect(mongoUri, mongooseOptions);
 //});
 
 // put all API endpoints under '/api/
-app.get('/api', (req, res) => {
-    res.status(200).json({ message: 'API is initialized!'});
-});
-
-app.get('/api/comments', (req, res) => {
-//   Comment.find(function(err, comments) {
-//        if (err) res.status(500).json(err);
-//        // responds with a json object of our database comments.
-//        res.status(200).json(comments);
-//    });
-    res.json({comments:[]});
-});
+//app.get('/api', (req, res) => {
+//    res.status(200).json({ message: 'API is initialized!'});
+//});
+//
+//app.get('/api/comments', (req, res) => {
+////   Comment.find(function(err, comments) {
+////        if (err) res.status(500).json(err);
+////        // responds with a json object of our database comments.
+////        res.status(200).json(comments);
+////    });
+//    res.json({comments:[]});
+//});
 
 //app.get('/', (req, res) => {
 //   res.sendFile(path.join(__dirname+'/build/index.html'));
