06bd829de9ba3081709f6085d32e2bfd7a2568eb
Eric Beyer <eric.m.beyer@gmail.com>
2017-10-24 23:28:58 -0700

    WIP - tryna get this to work

--- server.js
+++ server.js
@@ -1,33 +1,44 @@
 //server.js
 'use strict';
+
 //first we import our dependencies…
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var Comment = require('./src/model/comments');
+
 //and create our instances
 var app = express();
-var router = express.Router();
-//set our port to either a predetermined port number if you have set 
-//it up, or 3001
-var port = process.env.API_PORT || 3001;
-//db config
-mongoose.connect('mongodb://test_user:password@ds117495.mlab.com:17495/mern-comment-box');
 //now we should configure the API to use bodyParser and look for 
 //JSON data in the request body
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+var router = express.Router();
+//set our port to either a predetermined port number if you have set 
+//it up, or 8080
+var port = process.env.API_PORT || 8080;
+
+//db config
+var mongooseOptions = {
+  useMongoClient: true  
+};
+var mongoUri = process.env.MONGODB_URI || 'mongodb://test_user:password@ds117495.mlab.com:17495/mern-comment-box';
+mongoose.connect(mongoUri, mongooseOptions);
+
+// TODO - maybe we don't need this
 //To prevent errors from Cross Origin Resource Sharing, we will set 
 //our headers to allow CORS with middleware like so:
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-    //and remove cacheing so we get the most recent comments
-    res.setHeader('Cache-Control', 'no-cache');
-    next();
-});
+//app.use(function(req, res, next) {
+//    res.setHeader('Access-Control-Allow-Origin', '*');
+//    res.setHeader('Access-Control-Allow-Credentials', 'true');
+//    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+//    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+//    //and remove cacheing so we get the most recent comments
+//    res.setHeader('Cache-Control', 'no-cache');
+//    next();
+//});
+
 //now we can set the route path & initialize the API
 router.get('/', function(req, res) {
     res.json({ message: 'API Initialized!'});
@@ -83,8 +94,10 @@ router.route('/comments/:comment_id')
             res.json({ message: 'Comment has been deleted' });
         });
     });
+
 //Use our router configuration when we call /api
 app.use('/api', router);
+
 //starts the server and listens for requests
 app.listen(port, function() {
     console.log('api running on port ${port}');
